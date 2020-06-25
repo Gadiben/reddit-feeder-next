5d2245596b34aa2acdbee6956c15bb7510d0f99e
Gautier Dervaux <gautier_dervaux@hotmail.com>
2020-06-25 17:42:50 +0200

    add loading indicator

--- pages/search.js
+++ pages/search.js
@@ -3,8 +3,10 @@ import { useQuery } from "@apollo/react-hooks";
 import TextField from "@material-ui/core/TextField";
 import { withApollo } from "../libs/apollo";
 import InputLabel from "@material-ui/core/InputLabel";
-import Button from "@material-ui/core/Button";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
 import { POPULAR } from "../gql/querries";
 import { SubReddit } from "../components/SubReddit/subReddit";
 function Search() {
@@ -45,22 +47,32 @@ function Search() {
           Search
         </Button>
       </form>
-      {data
-        ? data.searchPopularReddit.map((subreddit) => {
-            // console.log(subreddit);
-            return (
-              <SubReddit
-                title={subreddit.title}
-                posts={subreddit.posts}
-              ></SubReddit>
-            );
-          })
-        : ""}
+      {data ? (
+        data.searchPopularReddit.map((subreddit) => {
+          // console.log(subreddit);
+          return (
+            <SubReddit
+              title={subreddit.title}
+              posts={subreddit.posts}
+            ></SubReddit>
+          );
+        })
+      ) : (
+        <div className="loading">
+          <CircularProgress />
+        </div>
+      )}
       <style jsx>
         {`
           .search-page {
             max-width: 1100px;
-            margin auto;
+            margin: auto;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+          .search-page > h1 {
+            align-self: flex-start;
           }
           .search-reddit {
             width: 60%;
@@ -74,6 +86,9 @@ function Search() {
             font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
               "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
               "Helvetica Neue", sans-serif;
+          }
+          .loading {
+            margin-top: 50px;
           }
         `}
       </style>
